12e176898421a974de5782cba50b7a6e45ef10a5
Mauro Stettler <mauro.stettler@gmail.com>
2019-01-04 09:48:24 -0300

    introduce objectIDs

--- sized_pool.go
+++ sized_pool.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bytes"
+	"reflect"
 	"syscall"
 )
 
@@ -11,13 +11,23 @@ type slab struct {
 	free freeList
 	data []byte
 }
+type objectID struct {
+	slabAddr  uintptr
+	objectPos uint8
+}
 
 type sizedPool struct {
 	slabs   []slab
 	objSize uint8
 }
 
-func (s *sizedPool) add(obj []byte) ([]byte, error) {
+// add adds an object to the pool
+// it will try to find a free object slot for the given object, to avoid
+// unnecessary allocations. if it can't find a free slot, it will add a
+// slab and then use that one.
+// the first return value is the objectID of the added object,
+// the second value is the error in case one occurred
+func (s *sizedPool) add(obj []byte) (objectID, error) {
 	var pos uint8
 	var success bool
 	var slabId int
@@ -28,12 +38,14 @@ func (s *sizedPool) add(obj []byte) ([]byte, error) {
 			break
 		}
 	}
-
+	id := objectID{
+		objectPos: pos,
+	}
 	var err error
 	if !success {
 		slabId, err = s.addSlab()
 		if err != nil {
-			return nil, err
+			return id, err
 		}
 	}
 
@@ -43,22 +55,61 @@ func (s *sizedPool) add(obj []byte) ([]byte, error) {
 		s.slabs[slabId].data[i+offset] = obj[i]
 	}
 
-	return s.slabs[slabId].data[offset : offset+int(s.objSize)], nil
+	id.slabAddr = reflect.ValueOf(s.slabs[slabId].data).Pointer()
+	return id, nil
 }
 
-func (s *sizedPool) search(searching []byte) ([]byte, bool) {
+// search searches for a byte slice that must have the length of
+// the slab's objectSize.
+// When found it returns the objectID and true,
+// otherwise the second returned value will be false
+func (s *sizedPool) search(searching []byte) (objectID, bool) {
+	var res objectID
+	if len(searching) != int(s.objSize) {
+		return res, false
+	}
+
 	for _, slab := range s.slabs {
+	OBJECT:
 		for i := uint8(0); i < objectsPerSlab; i++ {
 			offset := i * s.objSize
-			obj := slab.data[offset : offset+s.objSize]
-			if slab.free.isUsed(i) && bytes.Equal(searching, obj) {
-				return obj, true
+			if slab.free.isUsed(i) {
+				obj := slab.data[offset : offset+s.objSize]
+				for i := range obj {
+					if obj[i] != searching[i] {
+						continue OBJECT
+					}
+				}
+				res.objectPos = i
+				res.slabAddr = reflect.ValueOf(slab.data).Pointer()
+				return res, true
 			}
 		}
 	}
-	return nil, false
+	return res, false
 }
 
+// get retreives and object of the given objectID
+// the second returned value is true if the object was found,
+// otherwise it's false
+func (s *sizedPool) get(obj objectID) ([]byte, bool) {
+	// verify that the objectPos is inside the valid range
+	if obj.objectPos >= objectsPerSlab {
+		return nil, false
+	}
+
+	slabId := s.getSlabId(obj.slabAddr)
+	if slabId < 0 {
+		return nil, false
+	}
+
+	slab := s.slabs[slabId]
+	offset := int(obj.objectPos) * int(s.objSize)
+
+	return slab.data[offset : offset+int(s.objSize)], true
+}
+
+// addSlab adds another slab to the pool and initalizes the related structs
 func (s *sizedPool) addSlab() (int, error) {
 	data, err := syscall.Mmap(-1, 0, int(s.objSize)*int(objectsPerSlab), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_ANON|syscall.MAP_PRIVATE)
 	if err != nil {
@@ -69,4 +120,34 @@ func (s *sizedPool) addSlab() (int, error) {
 		free: newFreeList(objectsPerSlab),
 	})
 	return len(s.slabs) - 1, nil
+}
+
+// delete takes an objectID and deletes it from the sizedPool
+// on success it returns true, otherwise false
+func (s *sizedPool) delete(obj objectID) bool {
+	slabId := s.getSlabId(obj.slabAddr)
+	if slabId < 0 {
+		return false
+	}
+
+	slab := s.slabs[slabId]
+
+	// verify that the given arguments refer to an object within the size of the data slice
+	if len(slab.data) <= int(obj.objectPos)*int(s.objSize) {
+		return false
+	}
+
+	slab.free.setFree(obj.objectPos)
+	return true
+}
+
+// getSlabId looks up the ID for the slab referred to by a given pointer
+// the pointer must point at the first element of the slabs data slice
+func (s *sizedPool) getSlabId(addr uintptr) int {
+	for i, s := range s.slabs {
+		if reflect.ValueOf(s.data).Pointer() == addr {
+			return i
+		}
+	}
+	return -1
 }
--- sized_pool_test.go
+++ sized_pool_test.go
@@ -8,37 +8,60 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestAddingObject(t *testing.T) {
+func TestAddingGettingManyObjects(t *testing.T) {
 	sp := sizedPool{objSize: 5}
-	testString := "abcde"
-	Convey("When adding a byte slice to the pool", t, func() {
-		pooledBytes, _ := sp.add([]byte(testString))
-		Convey("the returned bytes should have the same value", func() {
-			So(pooledBytes, ShouldResemble, []byte(testString))
+	objects := make(map[string]objectID)
+	Convey("When generating a set of many test objects", t, func() {
+		var err error
+		// generate twice as many test object as there are objects per slab and add them to sizedPool
+		for i := 0; i < int(objectsPerSlab)*2; i++ {
+			value := fmt.Sprintf("%05d", i)
+			objects[value], err = sp.add([]byte(value))
+			So(err, ShouldBeNil)
+		}
+		Convey("We should be able to retreive each of them and get the correct value back", func() {
+			var returned []byte
+			var success bool
+			for value, objId := range objects {
+				returned, success = sp.get(objId)
+				So(success, ShouldBeTrue)
+				So(string(returned), ShouldEqual, value)
+			}
+			So(len(sp.slabs), ShouldEqual, 2)
 		})
 	})
 }
 
-func TestSearchingObject(t *testing.T) {
+func TestAddingSearchingObject(t *testing.T) {
 	sp := sizedPool{objSize: 5}
 	testString := "abcde"
+	testString2 := "aaaaa"
+	var result1, result2 objectID
+	var success bool
 	Convey("When adding a byte slice to the pool", t, func() {
-		pooledBytes, _ := sp.add([]byte(testString))
+		sp.add([]byte(testString))
 		Convey("we should be able to find it with the search method", func() {
-			result, success := sp.search([]byte("abcde"))
+			result1, success = sp.search([]byte(testString))
 			So(success, ShouldBeTrue)
-			So(result, ShouldResemble, pooledBytes)
-
-			// verify that pooledBytes and result have the same underlying data
-			pooledAddr := reflect.ValueOf(pooledBytes).Pointer()
-			resultAddr := reflect.ValueOf(result).Pointer()
-			So(resultAddr, ShouldEqual, pooledAddr)
+			So(sp.slabs[0].free.isUsed(result1.objectPos), ShouldBeTrue)
+			So(result1.slabAddr, ShouldEqual, reflect.ValueOf(sp.slabs[0].data).Pointer())
+		})
+	})
+	Convey("When adding a second object", t, func() {
+		sp.add([]byte(testString2))
+		Convey("we should also be able to find it", func() {
+			result2, success = sp.search([]byte(testString2))
+			So(success, ShouldBeTrue)
+			So(sp.slabs[0].free.isUsed(result2.objectPos), ShouldBeTrue)
+			So(result2.slabAddr, ShouldEqual, reflect.ValueOf(sp.slabs[0].data).Pointer())
 		})
 	})
 }
+
 func TestAddingMoreObjectsThanFitInOneSlab(t *testing.T) {
 	sp := sizedPool{objSize: 5}
 	Convey(fmt.Sprintf("When adding %d byte slices to the pool with %d objects per slab", objectsPerSlab+1, objectsPerSlab), t, func() {
+		// generating lots of 5-byte strings
 		for i := 10000; i <= 10000+int(objectsPerSlab); i++ {
 			toInsert := []byte(fmt.Sprintf("%d", i))
 			sp.add(toInsert)
